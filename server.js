4db85a788e6b1ead6a2deb93bb876e1f29db790c
yakattak <stevecalebrese@gmail.com>
2022-03-09 19:52:17 -0500

    connected to database

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const mysql = require('mysql2');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -11,6 +11,23 @@ app.use(express.json());
 app.use((req, res) => {
   res.status(404).end();
 });
+
+
+
+//connect to database
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //mysqlusername,
+        user: 'root',
+        //password
+        password: 'W&a94ixN%c2y3ho>^&3K',
+        database: 'election'
+
+    },
+    console.log('Connected to election database')
+)
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
