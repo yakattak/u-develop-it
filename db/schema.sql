47f213fb8d71dbf13aba655626517a151e71079e
yakattak <stevecalebrese@gmail.com>
2022-03-10 22:36:29 -0500

    voters

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS candidates;
 DROP TABLE IF EXISTS parties;
+DROP TABLE IF EXISTS voters;
 
 
 CREATE TABlE parties (
@@ -18,3 +19,10 @@ CREATE TABLE candidates (
 );
 
 
+CREATE TABLE voters (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
